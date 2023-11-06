12bbce0faf0b463a5295ca689ad49913561c71db
Babang <babang.dummy@gmail.com>
2023-11-06 16:31:46 +0700

    upload local repo to github

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const state = reactive({
+  count: 0
+})
+const increment = () => {
+  state.count++
+}
+</script>
+
+<template>
+  <h1>{{ state.count }}</h1>
+  <button @click="increment">Increment</button>
+</template>
